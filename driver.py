1d4e8e2f4647456f295c59e34d7829d210745a7b
Brandon Rachal <brandonrachalxula@gmail.com>
2025-11-17 12:49:38 -0600

    added student.py file, added the student class, also imported the student class to the driver.

--- driver.py
+++ driver.py
@@ -1,7 +1,30 @@
 import Transcript
+from student import Student
 
 def main():
     print("Driver code executed.")
+
+    student1 = Student(
+        name="Brandon Rachal",
+        classification="Junior",
+        major="Computer Science",
+        enrolled_classes=["CPSC 2735", "CPSC 2120", "Math 2050"],
+        minor="Business"
+    )
+
+    # Add grades
+    student1.add_grade("CPSC 2735", "A")
+    student1.add_grade("CPSC 2120", "B")
+    student1.add_grade("Math 2050", "A")
+
+    # Calculate GPA
+    student1.calculate_gpa()
+
+    # Display info
+    student1.display_info()
+
+    # Check for minor
+    print("Has a minor?", student1.has_minor())
 
 if __name__ == "__main__":
     main()
